63b4577c1cb1b1f83fb207a6e263d52d19296443
cmm9320 <carlos.maym@hotmail.com>
2016-10-24 16:05:52 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Floristeria</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all">
+<script type="text/javascript" src="js/jquery-1.6.js" ></script>
+<script type="text/javascript" src="js/cufon-yui.js"></script>
+<script type="text/javascript" src="js/cufon-replace.js"></script>  
+<script type="text/javascript" src="js/Forum_400.font.js"></script>
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="js/tms-0.3.js"></script>
+<script type="text/javascript" src="js/tms_presets.js"></script>
+<script type="text/javascript" src="js/script.js"></script>
+<script type="text/javascript" src="js/atooltip.jquery.js"></script> 
+
+<!--[if lt IE 9]>
+	<script type="text/javascript" src="js/html5.js"></script>
+	<style type="text/css">
+		.slider_bg {behavior:url(js/PIE.htc)}
+	</style>
+<![endif]-->
+<!--[if lt IE 7]>
+	<div style='clear:both;text-align:center;position:relative'>
+		<a href="http://www.microsoft.com/windows/internet-explorer/default.aspx?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" alt="" /></a>
+	</div>
+<![endif]-->
+</head>
+<body id="page1">
+<div class="body6">
+	<div class="body1">
+		<div class="body5">
+			<div class="main">
+<!-- header -->
+				<header>
+					<h1><a href="index.php" id="logo">Floristeria</a></h1>
+					<nav>
+						<ul id="top_nav">
+							<li><a href="index.php"><img src="images/icon_1.gif" alt=""></a></li>
+							<li><a href="#"><img src="images/icon_2.gif" alt=""></a></li>
+							<li class="end"><a href="Contacts.html"><img src="images/icon_3.gif" alt=""></a></li>
+						</ul>
+					</nav>
+					<nav>
+						<ul id="menu">
+							<li class="active"><a href="facturacion.html">Facturacion</a></li>
+							<li><a href="Wine.html">Gestiones</a></li>
+							<li><a href="CookBook.html">Reportes</a></li>
+							<li><a href="Contacts.html">Contactenos</a></li>
+						</ul>
+					</nav>
+				</header><div class="ic">More Website Templates  @ TemplateMonster.com - August 1st 2011!</div>
+<!-- / header -->
+<!-- content -->
+				<article id="content">
+					<div class="slider_bg">
+						<div class="slider">
+							<ul class="items">
+								<li>
+									<img src="images/img1.jpg" alt="">
+									<div class="banner">
+										<strong>Flores<span>de calidad</span></strong>
+										<b>Estilo</b>
+										<p>
+											<span>Nuestras flores son<br>
+											cultivadas en los mejores viveros del mundo bajo los mas altos estandares de calidad.</span>
+										</p>
+									</div>
+								</li>
+								<li>
+									<img src="images/img2.jpg" alt="">
+									<div class="banner">
+											<strong>Flores<span>de calidad</span></strong>
+										<b>Variedad</b>
+										<p>
+											<span>Haz de tu ocasion especial <br>
+											el mejor momento de tu vida.</span>
+										</p>
+									</div>
+								</li>
+								<li>
+									<img src="images/img3.jpg" alt="">
+									<div class="banner">
+											<strong>Flores<span>de calidad</span></strong>
+										<b>Calidad</b>
+										<p>
+											<span>Sorprende a tu persona especial <br>
+											Tenemos las mejores arreglos florales para consentir a esa persona especial.</span>
+										</p>
+									</div>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div class="wrap">
+						<section class="cols">
+							<div class="box">
+								<div>
+									<h2>Sucursales<span> y contacto</span> </h2>
+									<figure><img src="images/page1_img1.jpg" alt="" ></figure>
+									<p class="pad_bot1">City Mall Local 69 <br>Multiplaza Escazu Local 42 <br> Lincoln Plaza Local 71 <br> Terra Mall Local 56 <br><a href=facebook.com/Luxurious_Flowers>FB: Luxurious flowers</a>></p>
+								
+								</div>
+							</div>
+						</section>
+						<section class="cols pad_left1">
+							<div class="box">
+								<div>
+									<h2>Acerca de <span>Nosotros</span></h2>
+									<figure><img src="images/page1_img2.jpg" alt="" ></figure>
+									<p class="pad_bot1">Tenemos miles de formas de expresar nuestros sentimientos, ya sea un detalle de amor, agradecimiento o hasta ese día tan importante como lo es el día de su boda</p>
+								</div>
+							</div>
+						</section>
+						<section class="cols pad_left1">
+							<div class="box">
+								<div>
+									<h2>Nuestros <span>Servicios</span></h2>
+									<figure><img src="images/page1_img3.jpg" alt="" ></figure>
+									<ul class="list1 pad_bot1">
+										<li><a href="#">Arreglos florales y ramos</a></li>
+										<li><a href="#"></a>Bodas, cumpleaños</li>
+										<li><a href="#"></a>Funerales, amor y amistad</li>
+										<li><a href="#">Eventos empresariales</a></li>
+										<li><a href="#">Servicio a domicilio</a></li>
+									</ul>
+									
+								</div>
+							</div>
+						</section>
+					</div>
+				</article>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="body2">
+	<div class="main">
+		<article id="content2">
+			<div class="wrapper">
+				<section class="col1 pad_left1">
+					
+<div class="body3">
+	<div class="body4">
+		<div class="main">
+<!-- footer -->
+			<footer>
+				<div class="wrapper">
+					<section class="col1 pad_left1">
+						<h3>Linea gratuita: <span>1-800 2233 45 67</span></h3>
+						<a rel="nofollow" href="http://www.templatemonster.com/" target="_blank">Website Template</a> by TemplateMonster.com
+					</section>
+					<section class="col2 pad_left1">
+						<h3>Siganos en</h3>
+						<ul id="icons">
+							<li><a href="#" class="normaltip" title="Facebook"><img src="images/icon1.gif" alt=""></a></li>
+							<li><a href="#" class="normaltip" title="Linkedin"><img src="images/icon2.gif" alt=""></a></li>
+							<li><a href="#" class="normaltip" title="Twitter"><img src="images/icon3.gif" alt=""></a></li>
+							<li><a href="#" class="normaltip" title="Delicious"><img src="images/icon4.gif" alt=""></a></li>
+							<li><a href="#" class="normaltip" title="Technorati"><img src="images/icon5.gif" alt=""></a></li>
+						</ul>
+					</section>
+				</div>
+				<!-- {%FOOTER_LINK} -->
+			</footer>
+<!-- / footer -->
+		</div>
+	</div>
+</div>
+<script type="text/javascript"> Cufon.now(); </script>
+</body>
+</html>
